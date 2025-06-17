0f5ef83fdb45ab271a70cbd1c3d09fe8af72eea4
White ray <white.ray.haha@gmail.com>
2025-06-17 14:31:18 +0500

    Initial commit

---
+++ src/main/java/org/example/chat_with_websocket/controller/ChatController.java
@@ -0,0 +1,30 @@
+package org.javatechie.spring.ws.api.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.javatechie.spring.ws.api.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+@RequiredArgsConstructor
+public class ChatController {
+
+
+    @MessageMapping("/chat.register")
+    @SendTo("/topic/public")
+    public ChatMessage register(@Payload ChatMessage message, SimpMessageHeaderAccessor headerAccessor) {
+        headerAccessor.getSessionAttributes().put("username", message.getSender());
+        return message;
+    }
+
+    @MessageMapping("/chat.send")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+
+}
---
+++ src/test/java/org/example/chat_with_websocket/ChatWithWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javatechie.spring.ws.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatWithWebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
